9f4bfc00898a8273fd28d3fbab27824959f83cfb
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2024-07-14 17:24:15 +0200

    do not follow symbolic links when replacing them

--- install.sh
+++ install.sh
@@ -2,18 +2,18 @@
 
 DIRECTORY="$(realpath $(dirname -- "$0"))"
 
-ln -sf "$DIRECTORY/.zshrc" "$HOME/.zshrc"
+ln -nsf "$DIRECTORY/.zshrc" "$HOME/.zshrc"
 
-ln -sf "$DIRECTORY/.gitconfig" "$HOME/.gitconfig"
+ln -nsf "$DIRECTORY/.gitconfig" "$HOME/.gitconfig"
 
-ln -sf "$DIRECTORY/.tmux.conf" "$HOME/.tmux.conf"
+ln -nsf "$DIRECTORY/.tmux.conf" "$HOME/.tmux.conf"
 
 mkdir -p "$HOME/.config/Code/User"
-ln -sf "$DIRECTORY/.config/Code/User/settings.json" "$HOME/.config/Code/User/settings.json"
+ln -nsf "$DIRECTORY/.config/Code/User/settings.json" "$HOME/.config/Code/User/settings.json"
 
-ln -sf "$DIRECTORY/.config/alacritty" "$HOME/.config/alacritty"
+ln -nsf "$DIRECTORY/.config/alacritty" "$HOME/.config/alacritty"
 
-ln -sf "$DIRECTORY/.config/i3" "$HOME/.config/i3"
-ln -sf "$DIRECTORY/.config/i3status" "$HOME/.config/i3status"
+ln -nsf "$DIRECTORY/.config/i3" "$HOME/.config/i3"
+ln -nsf "$DIRECTORY/.config/i3status" "$HOME/.config/i3status"
 
-ln -sf "$DIRECTORY/bin" "$HOME/bin"
+ln -nsf "$DIRECTORY/bin" "$HOME/bin"
